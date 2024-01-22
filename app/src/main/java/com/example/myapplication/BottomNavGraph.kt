fd2623efd64782a6082120a9db233ed2df194a5f
Kickko <a01032762271@gmail.com>
2024-01-23 01:02:55 +0900

    [feat] init home screen with Staggered Grid

--- app/src/main/java/com/example/myapplication/BottomNavGraph.kt
+++ app/src/main/java/com/example/myapplication/BottomNavGraph.kt
@@ -3,6 +3,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.myapplication.BottomBarScreen
+import com.example.myapplication.screens.HomeScreen
 
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
--- app/src/main/java/com/example/myapplication/screens/home.kt
+++ app/src/main/java/com/example/myapplication/screens/home.kt
@@ -1,22 +1,91 @@
-import androidx.compose.foundation.layout.Box
+package com.example.myapplication.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.R
+import kotlin.random.Random
+
+data class ListItem(
+    val height: Dp,
+    val imageRes: Int,
+)
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    val imageResources = listOf<Int>(
+        R.drawable.nature_1,
+        R.drawable.nature_2,
+        R.drawable.nature_3,
+        R.drawable.nature_4,
+        R.drawable.nature_5,
+        R.drawable.nature_6,
+        R.drawable.nature_7,
+        R.drawable.nature_8,
+        R.drawable.nature_9,
+        R.drawable.nature_10,
+    )
+    val items = (1..100).map {
+        ListItem(
+            height = Random.nextInt(100, 300).dp,
+            imageRes = imageResources.random(),
+        )
+    }
+    LazyVerticalStaggeredGrid(
+        columns = StaggeredGridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(20.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
+        verticalItemSpacing = 20.dp
+    ) {
+        items(items.size) { index ->
+            val galleryItem = items[index]
+            GalleryImage(item = galleryItem)
+        }
+    }
+}
 
 @Composable
-fun HomeScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun GalleryImage(
+    item: ListItem,
+    modifier: Modifier = Modifier
+) {
+    val painter = painterResource(id = item.imageRes)
+    val description = null
+
+    Surface(
+        modifier = modifier
+            .shadow(elevation = 15.dp, shape = RoundedCornerShape(10))
+            .clip(RoundedCornerShape(10)),
     ) {
-        Text(
-            text = "Home Screen",
-            fontFamily = FontFamily.Serif,
-            fontSize = 22.sp
+        Image(
+            painter = painter,
+            contentScale = ContentScale.Crop,
+            contentDescription = description,
+            modifier = Modifier
+                .fillMaxSize()
+                .height(item.height)
+                .clip(RoundedCornerShape(10))
         )
     }
 }
