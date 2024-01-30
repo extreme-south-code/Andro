6bfb14c125502887cd532a3e947ef201ef02a55a
Kickko <a01032762271@gmail.com>
2024-01-30 14:49:00 +0900

    [feat] impl home-screen with photo picker

--- app/src/main/java/com/example/myapplication/screens/home.kt
+++ app/src/main/java/com/example/myapplication/screens/home.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication.screens
 
-<<<<<<< HEAD
 import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -31,38 +30,10 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
-=======
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.example.myapplication.R
->>>>>>> cced59b9962cabb84f342b694708d4397eb26a4f
 import kotlin.random.Random
 
 data class ListItem(
     val height: Dp,
-<<<<<<< HEAD
     val imageUri: Uri?,
 )
 @Composable
@@ -133,40 +104,6 @@ fun HomeScreen(modifier: Modifier = Modifier, maxSelectionCount: Int = 1) {
                 Log.d("LOG", "log")
                 GalleryImage(item = listItem)
             }
-=======
-    val imageRes: Int,
-)
-@Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
-    val imageResources = listOf<Int>(
-        R.drawable.nature_1,
-        R.drawable.nature_2,
-        R.drawable.nature_3,
-        R.drawable.nature_4,
-        R.drawable.nature_5,
-        R.drawable.nature_6,
-        R.drawable.nature_7,
-        R.drawable.nature_8,
-        R.drawable.nature_9,
-        R.drawable.nature_10,
-    )
-    val items = (1..100).map {
-        ListItem(
-            height = Random.nextInt(100, 300).dp,
-            imageRes = imageResources.random(),
-        )
-    }
-    LazyVerticalStaggeredGrid(
-        columns = StaggeredGridCells.Fixed(2),
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(20.dp),
-        horizontalArrangement = Arrangement.spacedBy(20.dp),
-        verticalItemSpacing = 20.dp
-    ) {
-        items(items.size) { index ->
-            val galleryItem = items[index]
-            GalleryImage(item = galleryItem)
->>>>>>> cced59b9962cabb84f342b694708d4397eb26a4f
         }
     }
 }
@@ -176,11 +113,7 @@ fun GalleryImage(
     item: ListItem,
     modifier: Modifier = Modifier
 ) {
-<<<<<<< HEAD
     val painter = rememberAsyncImagePainter(model = item.imageUri)
-=======
-    val painter = painterResource(id = item.imageRes)
->>>>>>> cced59b9962cabb84f342b694708d4397eb26a4f
     val description = null
 
     Surface(
