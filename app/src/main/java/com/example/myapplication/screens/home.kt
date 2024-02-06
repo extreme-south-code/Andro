98bb3ac05e2aeb57332bc484015598ce9b3db60f
Kickko <a01032762271@gmail.com>
2024-02-06 14:44:50 +0900

    [modify] give padding to button

--- app/src/main/java/com/example/myapplication/screens/home.kt
+++ app/src/main/java/com/example/myapplication/screens/home.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.items
@@ -132,7 +133,9 @@ fun HomeScreen(
             colors = ButtonDefaults.buttonColors(
                 containerColor = Color.LightGray,
                 contentColor = Color.Black,
-            )
+            ),
+            modifier = Modifier
+                .padding(8.dp)
         ) {
             Text(buttonText)
         }
